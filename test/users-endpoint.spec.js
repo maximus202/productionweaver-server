cecaa6fd718b239b14521fa6d32ebc7cdae0ba20
Max R <maxx.ratto@gmail.com>
2020-03-09 05:16:40 -0700

    Added test describe block for all GET /api/users endpoint and added new context block to test when there are no users in the database.

--- test/users-endpoint.spec.js
+++ test/users-endpoint.spec.js
@@ -21,62 +21,73 @@ describe.only('Users endpoint', function () {
     //Remove data from users table before all tests in this file.
     before('clean the tables', () => helpers.cleanTables(db))
 
-    //Tests for when there are users in the db
-    context('given there are users in the database', () => {
-        const testUsers = [
-            {
-                id: 1,
-                first_name: 'Martin',
-                last_name: 'Scorsese',
-                email: 'mscorsese@studio.com',
-                password: 'taxidriver',
-                date_created: '2029-01-22T16:28:32.615Z'
-            },
-            {
-                id: 2,
-                first_name: 'Alfred',
-                last_name: 'Hitchcock',
-                email: 'ahitchcock@studio.com',
-                password: 'psycho',
-                date_created: '2001-01-22T16:28:32.615Z'
-            },
-            {
-                id: 3,
-                first_name: 'Ridley',
-                last_name: 'Scott',
-                email: 'rscott@studio.com',
-                password: 'themartian',
-                date_created: '2001-01-28T16:28:32.615Z'
-            },
-            {
-                id: 4,
-                first_name: 'James',
-                last_name: 'Cameron',
-                email: 'jcameron@studio.com',
-                password: 'avatar',
-                date_created: '1992-07-22T16:28:32.615Z'
-            },
-            {
-                id: 5,
-                first_name: 'Quentin',
-                last_name: 'Tarantino',
-                email: 'qtarantino@studio.com',
-                password: 'reservoirdogs',
-                date_created: '1999-07-02T16:28:32.615Z'
-            },
-        ];
-
-        //Inserts testUsers into users table before each test in this context block
-        beforeEach('insert users', () => {
-            return db
-                .into('productionweaver_users')
-                .insert(testUsers)
+    describe('GET /api/users', () => {
+        //Tests for when there aren't users in the db
+        context('given there are no users in the database', () => {
+            it('GET /users responds with 200 and all users', () => {
+                return supertest(app)
+                    .get('/api/users')
+                    .expect(200, [])
+            })
         })
 
-        it('GET /users responds with 200 and all users', () => {
-            return supertest(app)
-                .get('/api/users/')
-                .expect(200, testUsers)
+        //Tests for when there are users in the db
+        context('given there are users in the database', () => {
+            const testUsers = [
+                {
+                    id: 1,
+                    first_name: 'Martin',
+                    last_name: 'Scorsese',
+                    email: 'mscorsese@studio.com',
+                    password: 'taxidriver',
+                    date_created: '2029-01-22T16:28:32.615Z'
+                },
+                {
+                    id: 2,
+                    first_name: 'Alfred',
+                    last_name: 'Hitchcock',
+                    email: 'ahitchcock@studio.com',
+                    password: 'psycho',
+                    date_created: '2001-01-22T16:28:32.615Z'
+                },
+                {
+                    id: 3,
+                    first_name: 'Ridley',
+                    last_name: 'Scott',
+                    email: 'rscott@studio.com',
+                    password: 'themartian',
+                    date_created: '2001-01-28T16:28:32.615Z'
+                },
+                {
+                    id: 4,
+                    first_name: 'James',
+                    last_name: 'Cameron',
+                    email: 'jcameron@studio.com',
+                    password: 'avatar',
+                    date_created: '1992-07-22T16:28:32.615Z'
+                },
+                {
+                    id: 5,
+                    first_name: 'Quentin',
+                    last_name: 'Tarantino',
+                    email: 'qtarantino@studio.com',
+                    password: 'reservoirdogs',
+                    date_created: '1999-07-02T16:28:32.615Z'
+                },
+            ];
+
+            //Inserts testUsers into users table before each test in this context block
+            beforeEach('insert users', () => {
+                return db
+                    .into('productionweaver_users')
+                    .insert(testUsers)
+            })
+
+            it('GET /users responds with 200 and all users', () => {
+                return supertest(app)
+                    .get('/api/users/')
+                    .expect(200, testUsers)
+            })
         })
     })
 })
