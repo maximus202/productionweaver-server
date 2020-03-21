e29fa4d6f18c45991f1451d3d916751f745aaa4d
Max R <maxx.ratto@gmail.com>
2020-03-21 14:26:00 -0700

    Added API_BASE_URL to config

--- src/config.js
+++ src/config.js
@@ -3,4 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/productionweaver',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000'
 }
