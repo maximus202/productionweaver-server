663a8e03494e81e3d92eb54b61a34602b6594a24
Max R <maxx.ratto@gmail.com>
2020-03-30 09:28:14 -0700

    Updates for deploying to production.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const knex = require('knex')
 const app = require('./app')
-const { PORT, DATABASE_URL } = require('./config')
+
+const { PORT, DATABASE_URL, NODE_ENV: environment } = require('./config')
 
 const db = knex({
     client: 'pg',
     connection: DATABASE_URL,
 })
+
+const options = { environment }
+const app = App(options)
 
 app.set('db', db)
 
