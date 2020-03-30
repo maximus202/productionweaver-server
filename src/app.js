5593855d4027faa640918b554f70313c3d58e822
Max R <maxx.ratto@gmail.com>
2020-03-30 11:52:25 -0700

    Reverted some changes.

--- src/app.js
+++ src/app.js
@@ -3,40 +3,43 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
+const { CLIENT_ORIGIN } = require('./config')
 const usersRouter = require('./users/users-router')
 const productionsRouter = require('./productions/productions-router')
 const scenesRouter = require('./scenes/scenes-router')
 const elementsRouter = require('./elements/elements-router')
 const authRouter = require('./auth/auth-router')
 
-const app = (options = {}) => {
-    const { environment = 'development' } = options
-    const app = express()
+const app = express()
 
-    const morganOption = (environment === 'production')
-        ? 'tiny'
-        : 'common';
+const morganOption = (NODE_ENV === 'production')
+    ? 'tiny'
+    : 'common';
 
-    app.use(morgan(morganOption))
-    app.use(helmet())
-    app.use(cors())
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
 
-    app.use('/api/users', usersRouter)
-    app.use('/api/productions', productionsRouter)
-    app.use('/api/scenes', scenesRouter)
-    app.use('/api/elements', elementsRouter)
-    app.use('/api/auth', authRouter)
+app.get('/', (req, res) => {
+    res.send('Hello, world!')
+})
+app.use('/api/users', usersRouter)
+app.use('/api/productions', productionsRouter)
+app.use('/api/scenes', scenesRouter)
+app.use('/api/elements', elementsRouter)
+app.use('/api/auth', authRouter)
 
-    app.use(function errorHandler(error, req, res, next) {
-        let response
-        if (environment === 'production') {
-            response = { error: { message: 'server error' } }
-        } else {
-            console.error(error)
-            response = { message: error.message, error }
-        }
-        res.status(500).json(response)
-    })
-}
+app.use(function errorHandler(error, req, res, next) {
+    let response
+    if (NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } }
+    } else {
+        console.error(error)
+        response = { message: error.message, error }
+    }
+    res.status(500).json(response)
+})
+
 
 module.exports = app
--- src/server.js
+++ src/server.js
@@ -1,15 +1,11 @@
 const knex = require('knex')
 const app = require('./app')
-
-const { PORT, DATABASE_URL, NODE_ENV: environment } = require('./config')
+const { PORT, DATABASE_URL } = require('./config')
 
 const db = knex({
     client: 'pg',
     connection: DATABASE_URL,
 })
-
-const options = { environment }
-const app = App(options)
 
 app.set('db', db)
 
